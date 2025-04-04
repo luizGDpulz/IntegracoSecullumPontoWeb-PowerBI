c432e5d4d414be6cd69ab833424d5b0b8fd0ad6e
Luiz Gustavo Dias Pulz <162385594+luizGDpulz@users.noreply.github.com>
2025-04-04 15:10:17 -0300

    Create getFuncionarios.m

---
+++ getFuncionarios.m
@@ -0,0 +1,25 @@
+let
+  getToken = Json.Document(
+      Web.Contents(
+          urlToken,
+          [
+              Headers = [#"Accept"="application/json", #"Content-Type"="application/x-www-form-urlencoded;charset=utf-8"],
+              Content = Text.ToBinary("grant_type=password&username="&username&"&password="&password&"&client_id=3")
+          ]
+  )),
+  token = getToken[access_token],
+  authToken = "Bearer " & token,
+  
+  getDados = Json.Document(
+      Web.Contents(
+          urlFuncionarios, 
+          [
+              Headers=[
+                  Authorization=authToken,
+                  secullumidbancoselecionado=id_banco
+              ]
+          ]
+      )
+  )
+in
+  getDados
